6acf965f0fdf98db07791d6bfca4e50829db17e8
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-14 13:28:33 -0400

    Adicionado o carousel das imgs dos restaurantes

---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const Card = styled.div`
+  width: 90px;
+  height: 90px;
+  border-radius: 6px;
+  background-image: url(${(props) => props.photo});
+  background-size: cover;
+`;
+
+const ImageCard = (props) => <Card photo={props.photo} />;
+
+export default ImageCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.svg";
-import { Container, Logo, Search, Wrapper, Map } from "./styles";
+import restaurante from "../../assets/restaurante-fake.png";
+import {
+  Container,
+  Logo,
+  Search,
+  Wrapper,
+  Map,
+  CaroulselTitle,
+} from "./styles";
 import TextField, { Input } from "@material/react-text-field";
 import MaterialIcon from "@material/react-material-icon";
+import Slider from "react-slick";
+import { Card } from "../../components";
 
 const Home = () => {
   const [inputText, setInputText] = useState("");
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
   return (
     <Wrapper>
       <Container>
@@ -23,6 +42,15 @@ const Home = () => {
               onChange={(e) => setInputText(e.target.value)}
             />
           </TextField>
+          <CaroulselTitle>Na sua Área</CaroulselTitle>
+          <Slider {...settings}>
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+            <Card photo={restaurante} />
+          </Slider>
         </Search>
       </Container>
       <Map />
