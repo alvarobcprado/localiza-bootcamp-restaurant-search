7caace7d204c1dd4c1a3e629fc686cacc4c2a99d
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-15 13:53:25 -0400

    Alterado parâmetros da API Google maps

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,9 +1,38 @@
+import React, { useState } from "react";
 import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
 const MapContainer = (props) => {
+  const [map, setMap] = useState(null);
   const { google } = props;
 
-  return <Map google={google} centerAroundCurrentLocation={true} />;
+  function searchNearBy(map, center) {
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      location: center,
+      radius: "30000",
+    };
+
+    return service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesService.OK) {
+        console.log("Restaurantes >>>>", results);
+      }
+    });
+  }
+
+  function onMapReady(_, map) {
+    setMap(map);
+    searchNearBy(map, map.center);
+  }
+
+  return (
+    <Map
+      google={google}
+      centerAroundCurrentLocation={true}
+      onReady={onMapReady}
+      onRecenter={onMapReady}
+    />
+  );
 };
 
 export default GoogleApiWrapper({
