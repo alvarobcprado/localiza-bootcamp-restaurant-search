17a278dfe4d6391bbe1be00ff055f290334d1633
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-15 10:04:26 -0400

    Criado o portal para o Modal Window

---
+++ src/components/Modal/Portal/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Portal = ({ children }) => {
+  const portal = document.getElementById("modal-root");
+  return ReactDOM.createPortal(children, portal);
+};
+
+export default Portal;
